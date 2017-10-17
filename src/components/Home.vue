875a718a9b4da1d63188c9fd8286bc738bfc6515
hexyxj <356061044@qq.com>
2017-10-17 12:32:53 +0800

    'banner'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,25 +2,24 @@
   <div class="home">
     <div class="banner" @click="menuChange">
       <div class="flex-box">
-        <div>
+       
           <router-link to="/recomend" class="menu-btn active">个性推荐</router-link>
-        </div>
+        
       </div>
       <div class="flex-box">
-        <div>
+        
           <router-link to="/music" class="menu-btn">歌单</router-link>
-
-        </div>
+       
       </div>
       <div class="flex-box">
-        <div>
+        
           <router-link to="/bs" class="menu-btn">主播电台</router-link>
-        </div>
+        
       </div>
       <div class="flex-box">
-        <div>
+        
           <router-link to="/rank" class="menu-btn">排行榜</router-link>
-        </div>
+        
       </div>
     </div>
     <router-view></router-view>
@@ -50,8 +49,6 @@ export default {
 .banner {
   border-bottom: 1px solid #ddd;
   display: flex;
-  padding-left: 15px;
-  padding-right: 15px;
 }
 
 .flex-box {
@@ -59,17 +56,11 @@ export default {
   text-align: center;
 }
 
-.flex-box>div {
-  padding-left: 25%;
-  width: 50%;
-  height: 100%;
-}
-
 .flex-box a {
   box-sizing: border-box;
-  display: block;
+  display:block;
   text-align: center;
-  font-size: 20px;
+  font-size: 18px;
   text-decoration: none;
   color: #000;
   padding: 15px;
--- src/components/home/Recomend.vue
+++ src/components/home/Recomend.vue
@@ -1,14 +1,104 @@
 <template>
-  <div class="video">
-     <!--  //视频 -->
-     <p>个性推荐</p>
+  <div class="recomend">
+    <!-- <p>个性推荐</p> -->
+    <div class="r-container">
+      <div class="slider">
+        <ul class="slider-img clearfix">
+          <li>
+            <a href="#"><img src="../../assets/img/1.png" alt=""></a>
+          </li>
+          <li>
+            <a href="#"><img src="../../assets/img/2.png" alt=""></a>
+          </li>
+          <li>
+            <a href="#"><img src="../../assets/img/3.png" alt=""></a>
+          </li>
+          <li>
+            <a href="#"><img src="../../assets/img/4.png" alt=""></a>
+          </li>
+        </ul>
+        <ul class="slider-inds">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div class="cards">
+        <div class="card-item">
+          <div class="card-btn">
+
+          </div>
+          <p class="card-title">私人FM</p>
+        </div>
+        <div class="card-item">
+          <div class="card-btn">
+
+          </div>
+          <p class="card-title">每日推荐</p>
+        </div>
+        <div class="card-item">
+          <div class="card-btn">
+
+          </div>
+          <p class="card-title">歌单</p>
+        </div>
+        <div class="card-item">
+          <div class="card-btn">
+
+          </div>
+          <p class="card-title">排行榜</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-    export default{
+export default {
 
-    }
+}
 </script>
-<style>
+<style lang="less">
+.recomend {
+  width: 100%;
+  height: 100%;
+}
 
+.slider {
+  position: relative;
+  overflow: hidden;
+}
+
+.slider-img {
+  display: flex;
+  width: 400%;
+}
+
+.slider-img li {
+  flex: 1;
+}
+
+.slider-inds {
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  bottom: 10px;
+  left: 0;
+}
+
+.slider-inds li {
+  display: inline-block;
+  width: 12px;
+  height: 12px;
+  border-radius: 6px;
+  background-color: #fff;
+}
+
+.slider-inds li:not(first-child) {
+  margin-right: 10px;
+}
+
+img {
+  width: 100%;
+}
 </style>
